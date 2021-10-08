86f6601ece7d8415b1b546ea91dfdf189db3272e
Nicholas Hayes <0xC0000054@users.noreply.github.com>
2021-10-08 17:45:36 -0600

    Upgrade the project to .NET 5

--- src/Properties/AssemblyInfo.cs
+++ src/Properties/AssemblyInfo.cs
@@ -12,6 +12,7 @@
 
 using System.Reflection;
 using System.Runtime.InteropServices;
+using System.Runtime.Versioning;
 
 // General Information about an assembly is controlled through the following
 // set of attributes. Change these attribute values to modify the information
@@ -32,6 +33,8 @@ using System.Runtime.InteropServices;
 
 // The following GUID is for the ID of the typelib if this project is exposed to COM
 [assembly: Guid("fbfe3f2e-91cb-40c7-bdd9-aace796d52dc")]
+
+[assembly: SupportedOSPlatform("windows")]
 
 // Version information for an assembly consists of the following four values:
 //
